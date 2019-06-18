66e4bcbd0f3d2a431eb7794be1b447925e54bf63
Sai Surya <upadrastasaisurya1@gmail.com>
2019-06-18 14:08:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -100,8 +100,8 @@ def sendEmail():
     # Create message container - the correct MIME type is multipart/alternative.
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Link"
-    msg['From'] = me
-    msg['To'] = you
+    msg['From'] = "{}".format(me)
+    msg['To'] = "{}".format(you)
 
     # Create the body of the message (a plain-text and an HTML version).
     text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
@@ -137,11 +137,9 @@ def sendEmail():
     </body>
     </html>
     """
-
     # Record the MIME types of both parts - text/plain and text/html.
     part1 = MIMEText(text, 'plain')
     part2 = MIMEText(html, 'html')
-
     # Attach parts into message container.
     # According to RFC 2046, the last part of a multipart message, in this case
     # the HTML message, is best and preferred.
@@ -149,11 +147,8 @@ def sendEmail():
     msg.attach(part2)
     # Send the message via local SMTP server.
     mail = smtplib.SMTP('smtp.office365.com', 587)
-
     mail.ehlo()
-
     mail.starttls()
-
     mail.login('meganb@M365x814387.onmicrosoft.com', 'mahgarg@2642')
     mail.sendmail(me, you, msg.as_string())
     mail.quit()
