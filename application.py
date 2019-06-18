4618ade1f2d5501d69e05ae6ac2f245b5a69101f
Sai Surya <upadrastasaisurya1@gmail.com>
2019-06-18 13:17:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -7,10 +7,14 @@ import logging
 import responses
 import json
 import sys
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from sqlalchemy.engine import create_engine
 
 app = Flask(__name__)
 c=0
-from sqlalchemy.engine import create_engine
+
 
 engine = create_engine(
     'postgres://%s:Amcompose2019@testamcompose.postgres.database.azure.com/postgres' % urllib.parse.quote(
@@ -88,3 +92,69 @@ def test():
 
     resp.headers['Content-Type'] = 'application/json'
     return resp
+@app.route("/sendEmail",methods=['POST'])
+def sendEmail():
+    qid = request.args.get('qid')
+    me = "meganb@M365x814387.onmicrosoft.com"
+    you = "meganb@M365x814387.onmicrosoft.com"
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Link"
+    msg['From'] = me
+    msg['To'] = you
+
+    # Create the body of the message (a plain-text and an HTML version).
+    text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+    html = ("\\n"
+            "    <html>\n"
+            "    <head>\n"
+            "      <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n"
+            "      <script type=\"application/adaptivecard+json\">{\n"
+            "        \"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\n"
+            "        \"originator\": \"863402fa-7924-43fa-a7e1-47293462aaf4\",\n"
+            "        \"type\": \"AdaptiveCard\",\n"
+            "        \"version\": \"1.0\",\n"
+            "        \"body\": [\n"
+            "            {\n"
+            "                \"type\": \"TextBlock\",\n"
+            "                \"spacing\": \"none\",\n"
+            "                \"isSubtle\": true,\n"
+            "                \"text\": \"Results will be displayed Soon...\"\n"
+            "            }\n"
+            "        ],\n"
+            "        \"autoInvokeAction\": {\n"
+            "            \"type\": \"Action.Http\",\n"
+            "            \"method\": \"POST\",\n"
+            "            \"hideCardOnInvoke\": false,\n"
+            "            \"url\": \"https://amcompose.azurewebsites.net/fetchLatestResponses\",\n"
+            "            \"body\": \""+qid+"\"\n"
+            "        }\n"
+            "    }\n"
+            "      </script>\n"
+            "    </head>\n"
+            "    <body>\n"
+            "    Visit the <a href=\"https://docs.microsoft.com/outlook/actionable-messages\">Outlook Dev Portal</a> to learn more about Actionable Messages.\n"
+            "    </body>\n"
+            "    </html>\n"
+            "    ")
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+    # Send the message via local SMTP server.
+    mail = smtplib.SMTP('smtp.office365.com', 587)
+
+    mail.ehlo()
+
+    mail.starttls()
+
+    mail.login('meganb@M365x814387.onmicrosoft.com', 'mahgarg@2642')
+    mail.sendmail(me, you, msg.as_string())
+    mail.quit()
