26d248792f571de66ad5e7c2aea560dff2f5289d
Sai Surya <upadrastasaisurya1@gmail.com>
2019-06-20 12:07:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -232,6 +232,11 @@ def fetchLatestResponses():
     return resp
 @app.route("/test",methods=['POST'])
 def test():
+    qid = request.data
+    qid = qid.decode("utf-8")
+    queryChoice = engine.execute("SELECT choice FROM question WHERE qid = %s", qid)
+    name = queryChoice.fetchall()
+    return str(name)
     t="{\n\"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\n\"originator\": \"863402fa-7924-43fa-a7e1-47293462aaf4\",\n\"type\": \"AdaptiveCard\",\n\"version\": \"1.0\",\n\"body\": [\n{\n\"type\": \"TextBlock\",\n\"spacing\": \"none\",\n\"isSubtle\": true,\n\"text\": \"lala\""
     t=t+"\n}\n],\n\"autoInvokeAction\": {\n\"type\": \"Action.Http\",\n\"method\": \"POST\",\n\"hideCardOnInvoke\": false,\n\"url\": \"https://amcomposetemplate.azurewebsites.net/test\",\n\"body\": \"{}\"\n}\n}"
     resp = Response(t)
