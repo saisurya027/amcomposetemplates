0b4da336d6954edb2852ae6ffdfae75da9d6ca72
Sai Surya <upadrastasaisurya1@gmail.com>
2019-06-24 14:55:33 +0530

    Update application.py

--- application.py
+++ application.py
@@ -275,8 +275,25 @@ def test():
 def sendEmail():
     qid = request.data
     qid = qid.decode("utf-8")
+    r = []
+    queryChoice = engine.execute("SELECT choice FROM question WHERE qid = %s", qid)
+    name = queryChoice.fetchall()
+
+    choices = str(name[0]).split(',')
+    choices[0] = choices[0][2:]
+    choices[len(choices) - 2] = choices[len(choices) - 2][:len(choices[len(choices) - 2]) - 1]
+    queryChoice = engine.execute("SELECT ques FROM question WHERE qid = %s", qid)
+    name = queryChoice.fetchall()
+    question = str(name[0])
+    for i in range(len(choices) - 1):
+        result = engine.execute("SELECT * FROM responses WHERE qid = %s and response= %s", (qid, choices[i]))
+        # r=r+choices[i]+"= "+ str(result.rowcount)
+        r.append(result.rowcount)
+    payload = generatePayload2(qid, question, choices, r)
+    payload = str(payload)
+
     me = "meganb@M365x814387.onmicrosoft.com"
-    you = "meganb@M365x814387.onmicrosoft.com"
+    you = "t-sais@micorosoft.com"
     # Create message container - the correct MIME type is multipart/alternative.
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Responses Of Your Poll"
@@ -288,25 +305,7 @@ def sendEmail():
     <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <script type="application/adaptivecard+json">{
-    "type": "AdaptiveCard",
-    "version": "1.0",
-    "originator": "863402fa-7924-43fa-a7e1-47293462aaf4",
-    "hideOriginalBody": true,
-    "body": [
-      {
-        "type": "TextBlock",
-        "text": "Responses will be displayed soon..."
-      }
-    ],
-    "autoInvokeAction": {
-        "type": "Action.Http",
-        "method": "POST",
-        "hideCardOnInvoke": false,
-        "url": "https://amcompose.azurewebsites.net/fetchLatestResponses",
-        "body": """+"\""+qid+"\""+"""
-    }
-  }
+  <script type="application/adaptivecard+json">"""+payload+"""
   </script>
 </head>
 <body>
@@ -314,6 +313,7 @@ Visit the <a href="https://docs.microsoft.com/outlook/actionable-messages">Outlo
 </body>
 </html>
     """
+    print(html, file=sys.stdout)
     # Record the MIME types of both parts - text/plain and text/html.
     part1 = MIMEText(text, 'plain')
     part2 = MIMEText(html, 'html')
